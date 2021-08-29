d7aecc14470133e8c0288de399b5a943de22ab70
MIchael Belete <it.michael.belete@gmail.com>
2021-08-29 19:31:22 -0400

    fetched post using prisma

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "24499d04375b31dc3296";
+/******/ 	var hotCurrentHash = "c3b3150d6523a66452d8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
