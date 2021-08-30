a9b9bfd4bebff13b38948a3bb6e1a91bf593bec9
MIchael Belete <it.michael.belete@gmail.com>
2021-08-29 20:02:45 -0400

    created post api and draft

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f8d4d7329195e0678418";
+/******/ 	var hotCurrentHash = "cdfba530202fc83cf637";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
